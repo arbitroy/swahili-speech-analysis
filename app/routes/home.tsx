29624ea70085ae7a8b9fd2c00d13cccff48e7917
arbitroy <austinndauwa@gmail.com>
2025-05-08 15:39:24 +0300

    mutuku conf

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import SpeechAnalysisApp, {  } from "../welcome/SpeechAnalysisApp";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Sema" },
+    { name: "description", content: "Mutuku's Problem" },
   ];
 }
 
